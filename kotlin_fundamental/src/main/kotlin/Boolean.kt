62531b5e82946710630d215078eef688ee3e3a87
lucypeng60 <68984174+lucypeng60@users.noreply.github.com>
2020-08-13 23:04:26 +0700

    Tugas kotlin fundamental pt2

---
+++ kotlin_fundamental/src/main/kotlin/Boolean.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val nilai = 75
+    val hasil : Char
+
+    hasil = if (nilai >90 && nilai <=100){
+        'A'
+    }else if (nilai >80 && nilai <=90){
+        'B'
+    }else if (nilai >70 && nilai <=80){
+        'C'
+    }else if (nilai >60 && nilai <=70){
+        'D'
+    }else {
+        'E'
+    }
+    println(hasil)
+}
